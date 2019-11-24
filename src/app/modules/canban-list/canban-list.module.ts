f28579c69dd725f1f61e9f69b9dede9d155bce4a
mezhik91 <mezhik91@gmail.com>
2019-11-25 00:02:59 +0100

    feat(Drag and Drop): Init basic dragging

--- src/app/modules/canban-list/canban-list.module.ts
+++ src/app/modules/canban-list/canban-list.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 import { StoreModule } from "@ngrx/store";
 import * as fromCanbanList from "./canban-list.reducer";
 import { EffectsModule } from "@ngrx/effects";
@@ -9,11 +10,13 @@ import { CanbanListEffects } from "./canban-list.effects";
   declarations: [],
   imports: [
     CommonModule,
+    DragDropModule,
     StoreModule.forFeature(
       fromCanbanList.canbanListFeatureKey,
       fromCanbanList.reducer
     ),
     EffectsModule.forFeature([CanbanListEffects])
-  ]
+  ],
+  exports: [DragDropModule]
 })
 export class CanbanListModule {}

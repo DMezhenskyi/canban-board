e9f58b20ed6a80d6a26e775937226691055665dc
mezhik91 <mezhik91@gmail.com>
2019-11-24 17:30:14 +0100

    fix(App): fix ts compilation issues

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AppEffects {
-  constructor(private actions$: Actions) {}
+  constructor() {}
 }
